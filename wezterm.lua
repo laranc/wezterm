a6e7baa15c95b018b8d8393189c7311ac7960101
laranc <laranc@github.com>
2025-02-20 18:03:51 +1030

    Add new font

--- wezterm.lua
+++ wezterm.lua
@@ -3,7 +3,7 @@ local config = {}
 
 config.color_scheme = "Gruvbox Dark (Gogh)"
 config.window_background_opacity = 0.9
-config.font = wezterm.font_with_fallback({ "Monocraft", "Fira Code Nerd Font" })
+config.font = wezterm.font_with_fallback({ "BigBlueTerm437 Nerd Font", "Monocraft Nerd Font" })
 config.font_size = 11.0
 config.window_frame = {
 	font = wezterm.font_with_fallback({ "Fixedsys Excelsior", "Roboto" }),
